373c9232715b1b9c77e8ede5fff021dc9e2c87a6
Richelle <richelle.ak@gmail.com>
2022-12-08 11:34:43 -0800

    pass fourth test to find size cost of a large pizza

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,8 @@ Pizza.prototype.sizeCost = function(){
    this.totalCost += 10
   }else if (this.size === "medium"){
     this.totalCost +=12;
+  }else if (this.size === "large"){
+    this.totalCost +=14;
   }
   return this.totalCost;
 } 
