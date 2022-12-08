9d25d2d9ca8124b9530d511dbca6acbaae9f3bd2
Richelle <richelle.ak@gmail.com>
2022-12-08 11:32:29 -0800

    pass third test

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,8 @@ function Pizza(toppings, size){
 Pizza.prototype.sizeCost = function(){
   if (this.size === "small"){
    this.totalCost += 10
+  }else if (this.size === "medium"){
+    this.totalCost +=12;
   }
   return this.totalCost;
 } 
