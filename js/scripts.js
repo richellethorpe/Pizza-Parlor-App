bb3a3042fee35652e9ea33dbaaeb02fef49333eb
Richelle <richelle.ak@gmail.com>
2022-12-09 09:24:12 -0800

    update UI

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,25 @@ function Pizza(size, toppings){
 }
 
 Pizza.prototype.sizeCost = function(){
-  if (this.size === "small"){
+  if (this.size === "Small"){
    this.totalCost += 10
-  }else if (this.size === "medium"){
+  }else if (this.size === "Medium"){
     this.totalCost +=12;
-  }else if (this.size === "large"){
+  }else if (this.size === "Large"){
     this.totalCost +=14;
   }
   return this.totalCost;
 } 
 
-//ninja turtle pizza toppings $1 "marshmallows" "pineapple" "chocolate-fudge" "olives" "jalapenos"
-//ninja turtle pizza toppings $2 "anchovies" "pepperoni" "clams" "smoked-ham" "slime"
+//ninja turtle pizza toppings $1 "marshmallows" "pineapple" "chocolate" "olives" "jalapenos"
+//ninja turtle pizza toppings $2 "anchovies" "pepperoni" "clams" "ham" "slime"
 
 Pizza.prototype.toppingCost = function() {
   let toppingsArray = this.toppings;
   toppingsArray.forEach(topper => {
-  if ((topper === "marshmallows") || (topper === "pineapple")|| (topper === "chocolate-fudge")|| (topper === "olives")|| (topper === "jalapenos")){
+  if ((topper === "Marshmallows") || (topper === "Pineapple")|| (topper === "Chocolate")|| (topper === "Olives")|| (topper === "Jalapenos")){
     this.totalCost += 1;
-  }else if ((topper === "anchovies") || (topper === "pepperoni")|| (topper === "clams")|| (topper === "smoked-ham")|| (topper === "slime")){
+  }else if ((topper === "Anchovies") || (topper === "Pepperoni")|| (topper === "Clams")|| (topper === "Ham")|| (topper === "Slime")){
     this.totalCost +=2;
   }
 })
@@ -38,12 +38,11 @@ Pizza.prototype.toppingCost = function() {
 function handleZaOrder(e){
   e.preventDefault();
   document.getElementById("pizza-form").setAttribute("class", "hidden");
-  document.querySelector("div#receipt").removeAttribute("class");
+  document.querySelector("div#receipt").removeAttribute("class", "hidden");
   const sizeSelection = document.querySelector("input[name='za-size']:checked").value;  
   const toppingSelections = document.querySelectorAll("input[name=za-topping]:checked");
-  console.log(toppingSelections);
+  
   const toppingInput = Array.from(toppingSelections);
-
   let toppingsArray = [];
   toppingInput.forEach (element => {
     toppingsArray.push(element.value);
@@ -62,8 +61,10 @@ function handleZaOrder(e){
     li.append(element);
     ul.append(li);
 
-  document.getElementById("totalCost").innerText = za.totalCost;  
+   
   })
+
+  document.getElementById("totalCost").innerText = za.totalCost; 
 };
 
 window.addEventListener("load", function() {
